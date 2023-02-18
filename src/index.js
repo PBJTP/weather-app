0c9595d42c240283ca536aef926d41f23b02d064
PBJTP <jplypick1990@gmail.com>
2023-02-17 18:47:14 -0800

    Add temp conversion functions. Add location change argument

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-async function getWeatherData(location) {
-    const rawData = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=2ce4c90e826af0f3a3f83a9bf293c425', {mode: "cors"});
+async function GetWeatherData(location) {
+    const rawData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2ce4c90e826af0f3a3f83a9bf293c425`, {mode: "cors"});
     const data = await rawData.json();
     console.log(data);
-    
+    console.log(getC(data.main.temp, data.name));
+    console.log(getF(data.main.temp, data.name));
+    return getC(data.main.temp, data.name), getF(data.main.temp, data.name);
 }
 
-getWeatherData();
+getWeatherData('seattle');
+
+function getC(temp, location) {
+    return`${Math.round(temp - 273.15)} degrees Celsius in ${location}`;
+}
+
+function getF(temp, location) {
+    return`${Math.round((temp - 273.15) * (9/5) + 32)} degrees Farenheit in ${location}`;
+}
+
+let weather = {
+    temp: getWeatherData('california')
+}
+
+console.log(weather.temp);
