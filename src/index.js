f2f6a8bc1bac0513cb7d5b8e6a93ab0de254042a
PBJTP <jplypick1990@gmail.com>
2023-02-18 23:04:57 -0800

    Fix HTML spans. Add render to DOM function. Clear data object(function can still generate)

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@
     let weather = {
         //Store data from openweather
         data: {
-            location: 'seattle',
-            tempF: 0,
-            tempC: 0,
-            feelsLikeF: 0,
-            feelsLikeC: 0,
-            report: '',
-            windSpeed: 0,
-            windDirection: 0,
-            humidity: 0,
+            //Data stored here
         },
         //get data and assign to data object
-        getData: async function () {
-            const x = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.data.location}&appid=2ce4c90e826af0f3a3f83a9bf293c425`, {mode: "cors"});
+        getData: async function (location) {
+            const x = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2ce4c90e826af0f3a3f83a9bf293c425`, {mode: "cors"});
             const y = await x.json();
             this.data.location = y.name;
             this.data.tempC = this.getTempC(y.main.temp);
@@ -25,6 +17,10 @@
             this.data.windSpeed = y.wind.speed;
             this.data.windDirection = y.wind.deg;
             this.data.report = y.weather[0].description;
+
+            console.log(y);
+            console.log(this.data)
+            this.render();
         },
         //convert kelvin temp to farenheit
         getTempF: function (temp) {
@@ -37,10 +33,34 @@
         //convert degrees to a direction, ie: northwest or south
         getWindDirection: function () {
             //do stuff with the degrees from openweather data
+        },
+        render: function () {
+            const domLocation = document.getElementById('location');
+            const domTempF = document.getElementById('tempF');
+            const domTempC = document.getElementById('tempC');
+            const domFeelsLikeF = document.getElementById('feelsLikeF');
+            const domFeelsLikeC = document.getElementById('feelsLikeC');
+            const domReport = document.getElementById('report');
+            const domWindSpeed = document.getElementById('windSpeed');
+            const domWindDirection = document.getElementById('windDirection');
+            const domHumidity = document.getElementById('humidity');
+
+            domLocation.textContent = this.data.location;
+            domTempF.textContent = this.data.tempF;
+            domTempC.textContent = this.data.tempC;
+            domFeelsLikeF.textContent = this.data.feelsLikeF;
+            domFeelsLikeC.textContent = this.data.feelsLikeC;
+            domHumidity.textContent = this.data.humidity;
+            domWindSpeed.textContent = this.data.windSpeed;
+            domWindDirection.textContent = this.data.windDirection;
+            domReport.textContent = this.data.report;
         }
     }
 
     //testing functions and data assignment
-    weather.getData();
+    weather.getData('cairo');
     console.log(weather.data);
+
 })();
+
+console.log(new Date());
